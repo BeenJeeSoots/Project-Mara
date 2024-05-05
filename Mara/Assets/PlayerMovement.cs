3640a7774c8577048adc567468c0f9bf464e2d8b
BeenJeeSoots <bdsutton@mtu.edu>
2024-05-04 21:48:27 -0400

    Full Animation
    Character animation is now in place, unique animations for moving left, right, up and down. Will plan on adding a casting animation in the future.

--- Mara/Assets/PlayerMovement.cs
+++ Mara/Assets/PlayerMovement.cs
@@ -12,11 +12,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 2.0f;
     [SerializeField] float jumpForce = 8.0f;
     public GameObject fishingRod;
+    public Animator animator;
 
 
     void Start()
     {
         
+        //Time.timeScale = 0f;  
+        animator.updateMode = AnimatorUpdateMode.UnscaledTime;
         GameObject player = GameObject.FindWithTag("Player");
         rb = player.GetComponent<Rigidbody2D>();
 
@@ -43,10 +46,20 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         transform.Translate(Vector3.up * Time.deltaTime * jumpForce);
+
     }
 
     private void FixedUpdate(){
         rb.velocity = new Vector2 (moveX, moveY);
+
+
+        animator.SetFloat("moveForwards", rb.velocity.y);
+        animator.SetFloat("moveBackwards", rb.velocity.y);
+        animator.SetFloat("moveRight", Math.Abs(rb.velocity.x));
+
+
+
+
 
         if (moveX != 0)
         {
